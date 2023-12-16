88b2d687c3b4618f1e067556561c28f471644c0c
Danny Ayoub <danny.a95@gmail.com>
2023-12-16 00:13:20 -0500

    well, needed now

--- site/src/app/page.tsx
+++ site/src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>new ayoubd.com coming soon</h1>
+      <h1>new ayoubd.com coming soon. now with 512mb ram</h1>
     </main>
   )
 }
