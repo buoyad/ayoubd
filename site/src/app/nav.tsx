02267b0141af87d5a14e5b091e81678fa131f3e1
Danny Ayoub <danny.a95@gmail.com>
2023-12-29 17:11:33 -0500

    wip nav

--- site/src/app/nav.tsx
+++ site/src/app/nav.tsx
@@ -7,15 +7,15 @@ export default function Nav() {
 
     return <>
         <Box style={styles.spacer} />
-        <Box
-            row={true}
-            gap="large"
-            style={styles.container}>
-            <Link href="/">Home</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/work">Work</Link>
+        <Box style={styles.container}>
+            <Box row gap="large">
+                <Link href="/">Home</Link>
+                <Link href="/blog">Blog</Link>
+                <Link href="/projects">Projects</Link>
+                <Link href="/work">Work</Link>
+            </Box>
         </Box>
+        <div style={styles.line} />
     </>
 }
 
@@ -29,5 +29,12 @@ const styles = styleSheet({
         maxWidth: 'var(--content-width)',
         backgroundColor: 'var(--color-background)'
     },
-    spacer: { height: '36px' }
+    spacer: { height: '36px' },
+    line: {
+        width: '100%',
+        position: 'sticky',
+        top: 'calc(1lh + 24px)',
+        height: 1,
+        background: 'linear-gradient(90deg, rgba(var(--color-text-rgb), .2) 0%, rgba(var(--color-text-rgb), .3) 50%, rgba(var(--color-text-rgb), .2) 100%)'
+    }
 })
