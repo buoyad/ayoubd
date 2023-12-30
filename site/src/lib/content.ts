1a585809d4212a707d648b0e555d97ff18ba475a
Danny Ayoub <danny.a95@gmail.com>
2023-12-30 11:00:50 -0500

    test mdx cache performance

--- site/src/lib/content.ts
+++ site/src/lib/content.ts
@@ -1,12 +1,18 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 import path from 'path'
 import { compileMDX } from 'next-mdx-remote/rsc'
 import components from '@/ui/components/mdx'
 import rehypeMdxCodeProps from 'rehype-mdx-code-props'
 import rehypeHighlight from 'rehype-highlight'
+import { unstable_cache } from 'next/cache'
 
 const blogContentDir = path.join(process.cwd(), 'content-blog')
 const projectsContentDir = path.join(process.cwd(), 'content-projects')
+
+// this cache is only ever invalidated on deploys to production
+const cache = process.env.NODE_ENV === 'production' ?
+    unstable_cache :
+    <T extends Function>(fn: T) => fn
 
 export type FrontMatter = {
     title: string,
@@ -27,25 +33,28 @@ const compile = async (source: string) => {
     })
 }
 
-const getMDXFiles = (dir: string) => {
-    return fs.readdirSync(dir).filter(file => path.extname(file) === '.mdx')
-}
+const getMDXFiles = cache(async (dir: string) => {
+    return (await fs.readdir(dir))
+        .filter(file => path.extname(file) === '.mdx')
+})
 
-const readMDXFile = async (filePath: string) => {
-    const source = fs.readFileSync(filePath, 'utf8')
+const readMDXFile = cache(async (filePath: string) => await fs.readFile(filePath, 'utf8'))
+
+const compileMDXFile = async (filePath: string) => {
+    const source = await readMDXFile(filePath)
     const { content, frontmatter } = await compile(source)
     return { content, frontmatter }
 }
 
 const getMDXContent = async (dir: string) => {
-    const content = await Promise.all(getMDXFiles(dir).map(async (file) => {
+    const content = await Promise.all((await getMDXFiles(dir)).map(async (file) => {
         const filePath = path.join(dir, file)
-        const { content, frontmatter } = await readMDXFile(filePath)
+        const { content, frontmatter } = await compileMDXFile(filePath)
         return { content, frontmatter, slug: encodeURIComponent((frontmatter.slug || frontmatter.title).replace(/\s/g, '-')) }
     }))
     content.sort((a, b) => (a.frontmatter.published > b.frontmatter.published) ? -1 : 1)
     return content
 }
 
-export const getBlogContent = async () => await getMDXContent(blogContentDir)
-export const getProjectsContent = async () => await getMDXContent(projectsContentDir)
+export const getBlogContent = (async () => await getMDXContent(blogContentDir))
+export const getProjectsContent = (async () => await getMDXContent(projectsContentDir))
