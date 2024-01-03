d5f069e4640cf2c610d9d37b480a76f8a1024996
Danny Ayoub <danny.a95@gmail.com>
2024-01-03 00:14:41 -0500

    add tailwind and prettier deps and set up config for both

--- site/src/app/footer.tsx
+++ site/src/app/footer.tsx
@@ -1,29 +1,29 @@
 'use client'
-import { Box } from "../ui/components"
-import { styleSheet } from "../ui/util"
-import { ColorMode, useTheme } from "../ui/theme-context"
+import { Box } from '../ui/components'
+import { styleSheet } from '../ui/util'
+import { ColorMode, useTheme } from '../ui/theme-context'
 
 export default function Footer() {
-    const { theme } = useTheme()
-    return (
-        <footer style={styles.footer}>
-            <Box row style={styles.container} gap="none">
-                <ColorMode />
-            </Box>
-        </footer>
-    )
+  const { theme } = useTheme()
+  return (
+    <footer style={styles.footer}>
+      <Box row style={styles.container} gap="none">
+        <ColorMode />
+      </Box>
+    </footer>
+  )
 }
 
 const styles = styleSheet({
-    footer: {
-        borderTop: '1px solid var(--color-dimBorder)',
-        width: '100%',
-    },
-    container: {
-        margin: '0 auto',
-        width: 'var(--content-width)',
-        height: 'var(--footer-height)',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  footer: {
+    borderTop: '1px solid var(--color-dimBorder)',
+    width: '100%',
+  },
+  container: {
+    margin: '0 auto',
+    width: 'var(--content-width)',
+    height: 'var(--footer-height)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
