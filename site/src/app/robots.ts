be7a8aaccbe4896ae17d51c95300f4843c4152ee
Danny Ayoub <danny.a95@gmail.com>
2024-01-03 11:10:45 -0500

    add some disallows to robots.txt

--- site/src/app/robots.ts
+++ site/src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin'],
+      disallow: ['/admin', '/docs', '/fonts', '/models', '/sprites'],
     },
     sitemap: 'https://ayoubd.com/sitemap.xml',
   }
